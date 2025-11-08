05e5bf3c302f28835c18f0127f0e83eebe9a87b0
little_q_exist <3061982805@qq.com>
2025-11-08 11:51:54 +0800

    feat: 点击下一个按钮隐藏翻译信息，点击已知或未知按钮时显示翻译信息

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,32 @@ import words from './word'
 import Card from './components/Card'
 import ButtonGroup from './components/ButtonGroup'
 import { useRef, useState } from 'react'
+import Button from './components/Button'
 
 const App = () => {
     const [index, setIndex] = useState(0)
+
+    /* 
+        当信息隐藏时，展示“知道”和“不知道”按钮
+        当信息显示时，展示“下一个”按钮
+    */
     const [shouldShowInfo, setShouldShowInfo] = useState(false)
 
     const visibilityRef = useRef(null)
 
-    const handleKnown = () => {
-        visibilityRef.current.toggleVisibility()
+    const handleNextClick = () => {
         const nextIndex = (index + 1) % words.length
         setIndex(nextIndex)
     }
 
+    const handleKnown = () => {
+        visibilityRef.current.toggleVisibility()
+        setShouldShowInfo(true)
+    }
+
     const handleUnknown = () => {
         visibilityRef.current.toggleVisibility()
-        const nextIndex = (index + 1) % words.length
-        setIndex(nextIndex)
+        setShouldShowInfo(true)
     }
 
     return (
@@ -27,7 +36,12 @@ const App = () => {
 
             <Card word={words[index]} ref={visibilityRef} />
 
-            <ButtonGroup handleKnown={handleKnown} handleUnknown={handleUnknown} />
+            {!shouldShowInfo && (<ButtonGroup handleKnown={handleKnown} handleUnknown={handleUnknown} />)}
+            {shouldShowInfo && (<Button label={'Next'} onClick={() => {
+                handleNextClick()
+                setShouldShowInfo(false)
+                visibilityRef.current.toggleVisibility()
+            }} />)}
         </div>
     )
 }
