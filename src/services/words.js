cd3bdd2428cc0fc1607a552e1b23eb72ebd11b19
little_q_exist <3061982805@qq.com>
2026-01-26 12:30:18 +0800

    refactor: 把APIURL重构为后端API地址

--- src/services/words.js
+++ src/services/words.js
@@ -1,15 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const APIURL = 'http://localhost:4000/words'
+const APIURL = 'http://localhost:3000/api/words';
 
 const getALL = async () => {
-    const response = await axios.get(APIURL)
-    return response.data
-}
+    const response = await axios.get(APIURL);
+    return response.data;
+};
 
 const update = async (word) => {
-    const response = await axios.put(`${APIURL}/${word.id}`, word)
-    return response.data
-}
+    const response = await axios.put(`${APIURL}/${word.id}`, word);
+    return response.data;
+};
 
-export default { getALL, update }
+export default { getALL, update };
