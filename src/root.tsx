a7ca9b28dd17c55f725eb909b0cb3dec198741b7
little_q_exist <3061982805@qq.com>
2026-02-11 16:26:56 +0800

    feat: 增加重置整个页面为全屏的样式

--- src/root.tsx
+++ src/root.tsx
@@ -1,15 +1,23 @@
 import { Outlet, Scripts, ScrollRestoration } from 'react-router';
 
 export function Layout({ children }: { children: React.ReactNode }) {
+    const resetStyle = {
+        margin: 0,
+        padding: 0,
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden',
+    };
+
     return (
-        <html lang="en">
+        <html lang="en" style={resetStyle}>
             <head>
                 <title>recite-word</title>
                 <meta charSet="UTF-8" />
                 <link rel="icon" type="image/svg+xml" href="/vite.svg" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
-            <body>
+            <body style={resetStyle}>
                 {children}
                 <ScrollRestoration />
                 <Scripts />
