feddcfadc08ad397d23471c6cb0914b078157298
little_q_exist <3061982805@qq.com>
2026-02-11 17:47:57 +0800

    feat: 水平垂直居中卡片内容

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -10,22 +10,34 @@ interface WordInfoProps {
 }
 
 const WordInfo = ({ word, visible }: WordInfoProps) => {
-    const cardStyle = { minWidth: '15rem', width: '49.5%', height: '90%' };
+    const cardStyle: React.CSSProperties = {
+        minWidth: '15rem',
+        width: '49.5%',
+        height: '90%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+    };
 
-    const showWhenVisible = { display: visible ? '' : '' };
+    // debug: modify it to none
+    const showWhenVisible: React.CSSProperties = { opacity: visible ? 1 : 1 };
 
     return (
-        <div style={{ width: '100%', height: '75vh' }}>
+        <div style={{ width: '100%', height: '95%' }}>
             <Flex align="center" justify="space-between" style={{ width: '100%', height: '100%' }}>
-                <Card style={cardStyle}>
-                    <Space align="end" size="large">
+                <Card style={cardStyle} variant="borderless">
+                    <Space align="end" size="large" style={{ flex: 1 }}>
                         <Title level={1} style={{ margin: 0 }}>
                             {word.english}
                         </Title>
                         <Text style={{ fontSize: 20, padding: '0.5em 0' }}>{word.phonetic}</Text>
                     </Space>
                 </Card>
-                <Card style={{ ...cardStyle, ...showWhenVisible }}>
+                <Card
+                    style={{ ...cardStyle, ...showWhenVisible, textAlign: 'center' }}
+                    variant="borderless"
+                >
                     {word.definitions.map((definition, index) => (
                         <div key={index}>
                             {definition.partOfSpeech} {definition.meaning}
