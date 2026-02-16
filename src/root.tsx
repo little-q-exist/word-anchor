d9adc485725917ec28bb198816bd407601dc32e7
little_q_exist <3061982805@qq.com>
2026-02-16 21:51:50 +0800

    feat: 添加错误边界

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,6 @@
-import { Outlet, Scripts, ScrollRestoration } from 'react-router';
+import { Button, Result, Skeleton } from 'antd';
+import { isRouteErrorResponse, Link, Outlet, Scripts, ScrollRestoration } from 'react-router';
+import type { Route } from './+types/root';
 
 export function Layout({ children }: { children: React.ReactNode }) {
     const resetStyle = {
@@ -27,7 +29,37 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export function HydrateFallback() {
-    return <div>Loading...</div>;
+    return <Skeleton active />;
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+    if (isRouteErrorResponse(error)) {
+        return (
+            <>
+                <Result
+                    status="error"
+                    title={`${error.status} ${error.statusText}`}
+                    subTitle={`${error.data}`}
+                    extra={[
+                        <Button type="default" key="home">
+                            <Link to="..">Go Back</Link>
+                        </Button>,
+                    ]}
+                ></Result>
+            </>
+        );
+    } else if (error instanceof Error) {
+        return (
+            <div>
+                <h1>Error</h1>
+                <p>{error.message}</p>
+                <p>The stack trace is:</p>
+                <pre>{error.stack}</pre>
+            </div>
+        );
+    } else {
+        return <h1>Unknown Error</h1>;
+    }
 }
 
 const App = () => {
