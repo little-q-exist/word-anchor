e5048908b2b9fd8b941b0cf3a285e9892a332dc3
little_q_exist <3061982805@qq.com>
2026-02-19 22:43:41 +0800

    fix: 修改了菜单高亮不跟随url的问题

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,6 @@
-import { Avatar, Button, Layout, Menu, type MenuProps } from 'antd';
+import { Avatar, Layout, Menu, type MenuProps } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, Outlet } from 'react-router';
+import { Link, Outlet, useLocation } from 'react-router';
 import type { RootState } from '../store';
 import { logout } from '../features/userSlice';
 
@@ -11,46 +11,61 @@ type MenuItem = Required<MenuProps>['items'][number];
 const BackgroundLayout = () => {
     const user = useSelector((state: RootState) => state.user);
     const dispatch = useDispatch();
+    const location = useLocation();
 
-    const loginItem: MenuItem = { label: <Link to={`/login`}>LOGIN</Link>, key: 'login' };
+    const loginItem: MenuItem = { label: <Link to={`/login`}>LOGIN</Link>, key: '/login' };
 
     const userItem: MenuItem = {
         label: (
-            <Avatar style={{ backgroundColor: '#1677ff' }}>{user ? user.username : 'User'}</Avatar>
+            <Avatar style={{ backgroundColor: '#1677ff' }}>
+                {user ? user.username.slice(0, 3) : 'User'}
+            </Avatar>
         ),
         key: 'user',
         children: [
             {
-                label: <Button onClick={() => dispatch(logout())}>Logout</Button>,
+                label: <a>Profile</a>,
+                key: '/profile',
+            },
+            {
+                label: <a>Settings</a>,
+                key: '/settings',
+            },
+            { type: 'divider' },
+            {
+                label: <a onClick={() => dispatch(logout())}>Logout</a>,
                 key: 'logout',
             },
         ],
     };
 
     const menuItems: MenuItem[] = [
-        { label: <Link to={`/learn`}>LEARN</Link>, key: 'learn' },
+        { label: <Link to={`/learn`}>LEARN</Link>, key: '/learn' },
         user ? userItem : loginItem,
     ];
 
     return (
         <Layout style={{ height: '100%' }}>
-            <Header style={{ display: 'flex', alignItems: 'center' }}>
+            <Header style={{ display: 'flex', alignItems: 'center', backgroundColor: '#fff' }}>
                 <div
                     style={{
-                        backgroundColor: 'grey',
+                        backgroundColor: '#f9fafb',
                         borderRadius: '5px',
                         width: '5rem',
                         margin: '.7rem 1rem',
                         display: 'block',
+                        textAlign: 'center',
+                        lineHeight: '32px',
+                        color: 'black',
                     }}
                 >
-                    LOGO
+                    <Link to="..">LOGO</Link>
                 </div>
                 <Menu
                     mode="horizontal"
                     items={menuItems}
-                    theme="dark"
-                    style={{ flex: 1, minWidth: 0 }}
+                    style={{ flex: 1, minWidth: 0, justifyContent: 'flex-end' }}
+                    selectedKeys={[location.pathname]}
                 />
             </Header>
             <Content style={{ margin: '0 1.5rem' }}>
