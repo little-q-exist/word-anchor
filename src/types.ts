af4f0442eb766a665f974a1ddc988c29935502ff
little_q_exist <3061982805@qq.com>
2026-02-24 17:31:16 +0800

    feat: 新增了新单词的类型定义 refactor: 更新了学习记录的类型定义

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,15 @@ interface Definition {
     partOfSpeech: string;
 }
 
+export interface NewWord {
+    definitions: Definition[];
+    english: string;
+    exampleSentence: string[];
+    phonetic: string;
+    related: string[];
+    tags: string[];
+}
+
 export interface Word {
     definitions: Definition[];
     english: string;
@@ -11,17 +20,19 @@ export interface Word {
     related: string[];
     tags: string[];
     _id: string;
+    createdBy: string;
 }
 
 export interface UserLearningData {
-    familiarity: 0 | 1 | 2 | 3;
-    mastered: boolean;
-
-    favorited: boolean;
-
+    userId: string;
     wordId: string;
+    easeFactor: number;
+    lastLearned: string;
+    interval: number;
+    dueDate: string;
+    repetition: number;
+    favorited: boolean;
 }
-
 export interface NewUser {
     username: string;
     email?: string;
