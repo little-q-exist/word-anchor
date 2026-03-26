45371ebd8a132f38acafa3bc75bc1954689c7c20
little_q_exist <3061982805@qq.com>
2026-03-26 18:06:02 +0800

    fix: 修复钩子可能会返回未定义的问题

--- src/modules/word-learning/hooks/LearnWord/useWordCache.ts
+++ src/modules/word-learning/hooks/LearnWord/useWordCache.ts
@@ -3,19 +3,16 @@ import type { BriefWordWithLearnStatus } from '@/types';
 import localforage from 'localforage';
 import { useSelector } from 'react-redux';
 
-const useWordCache = (
-    mode: 'learn' | 'review' | undefined,
-    briefWords: BriefWordWithLearnStatus[]
-) => {
+const useWordCache = (mode: 'learn' | 'review' | undefined) => {
     console.log(localforage);
     const userId = useSelector((state: RootState) => state.user?._id);
-    if (!mode || briefWords.length === 0 || !userId) {
-        return;
-    }
 
-    const cacheKey = `${userId}-${mode}-briefWords`;
+    const cacheKey = `${userId ?? 'guest'}-${mode}-briefWords`;
 
     const getCache = async (): Promise<BriefWordWithLearnStatus[] | null> => {
+        if (!userId || !mode) {
+            return null;
+        }
         try {
             const cachedWords = await localforage.getItem<BriefWordWithLearnStatus[]>(cacheKey);
             return cachedWords || null;
@@ -25,7 +22,13 @@ const useWordCache = (
         }
     };
 
-    const setCache = async () => {
+    const setCache = async (briefWords: BriefWordWithLearnStatus[]) => {
+        if (!briefWords || briefWords.length === 0) {
+            return;
+        }
+        if (!userId || !mode) {
+            return;
+        }
         const cachedWords = await localforage.getItem<BriefWordWithLearnStatus[]>(cacheKey);
         if (!cachedWords) {
             try {
@@ -37,6 +40,9 @@ const useWordCache = (
     };
 
     const removeCache = async () => {
+        if (!userId || !mode) {
+            return;
+        }
         try {
             await localforage.removeItem(cacheKey);
         } catch (error) {
