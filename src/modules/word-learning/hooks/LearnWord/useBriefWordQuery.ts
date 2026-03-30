d4946eff642a1cdbaf61ac12faf5f47d15e01a18
little_q_exist <3061982805@qq.com>
2026-03-30 15:51:51 +0800

    refactor: 将获取简略单词的钩子模块化

---
+++ src/modules/word-learning/hooks/LearnWord/useBriefWordQuery.ts
@@ -0,0 +1,35 @@
+import type { BriefWordWithLearnStatus } from '@/types';
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import wordServices from '@/shared/services/words';
+
+const useBriefWordQuery = (enable: boolean, mode: 'learn' | 'review') => {
+    const briefWordQuery = useQuery({
+        queryKey: ['learnWords', mode],
+        queryFn: () =>
+            mode === 'learn' ? wordServices.getWordToLearn() : wordServices.getWordToReview(),
+        refetchOnWindowFocus: false,
+        enabled: enable,
+    });
+    const briefWordsWithStatus: BriefWordWithLearnStatus[] = useMemo(() => {
+        const loadedWords = briefWordQuery.data?.words || [];
+        return (
+            loadedWords.map((briefWord) => {
+                return { ...briefWord, status: 'idle' };
+            }) || []
+        );
+    }, [briefWordQuery.data?.words]);
+
+    const isBriefWordLoading = briefWordQuery.status === 'pending';
+    const isBriefWordError = briefWordQuery.status === 'error';
+    const canShowBriefWord = !!(briefWordQuery.status === 'success' && briefWordQuery.data);
+
+    return {
+        briefWordsWithStatus,
+        isBriefWordLoading,
+        isBriefWordError,
+        canShowBriefWord,
+    };
+};
+
+export default useBriefWordQuery;
