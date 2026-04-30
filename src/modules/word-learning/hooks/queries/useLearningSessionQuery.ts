06883141a79fef6d2c65ae86cafa3f1c86835f9d
little_q_exist <3061982805@qq.com>
2026-04-30 18:20:44 +0800

    refactor: 移除 queryOptions 调用

--- src/modules/word-learning/hooks/queries/useLearningSessionQuery.ts
+++ src/modules/word-learning/hooks/queries/useLearningSessionQuery.ts
@@ -1,6 +1,5 @@
 import learningSessionServices from '@modules/word-learning/services/learningSession';
 import useSuccessQuery from './useSuccessQuery';
-import { queryOptions } from '@tanstack/react-query';
 import { useDispatch } from 'react-redux';
 import { toNextStep } from '@/features/LearnWordSlice';
 
@@ -10,13 +9,12 @@ const useLearningSessionQuery = (
     enable: boolean = true
 ) => {
     const dispatch = useDispatch();
-    const learningSessionQuery = useSuccessQuery(
-        queryOptions({
+    const learningSessionQuery = useSuccessQuery({
             queryKey: ['learningSession', userId, mode],
             enabled: enable && !!userId,
             queryFn: () => learningSessionServices.getLearningSession(userId!, mode),
             refetchOnWindowFocus: false,
-        }),
+        },
         'fetchingSession',
         () => dispatch(toNextStep({ hasSession: true }))
     );
