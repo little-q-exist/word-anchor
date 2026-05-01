85e929a035bb0b6255f35b5eeda76505f74d2650
little_q_exist <3061982805@qq.com>
2026-05-01 15:01:06 +0800

    refactor: 使用 useSuccessQuery 替代 useQuery

--- src/modules/word-learning/hooks/queries/useDetailedWordQuery.ts
+++ src/modules/word-learning/hooks/queries/useDetailedWordQuery.ts
@@ -1,16 +1,24 @@
-import { useQuery, skipToken } from '@tanstack/react-query';
+import { skipToken } from '@tanstack/react-query';
 import wordServices from '@modules/word-core/services/words';
+import useSuccessQuery from './useSuccessQuery';
+import { toNextStep } from '@/features/LearnWordSlice';
+import { useDispatch } from 'react-redux';
 
 const useDetailedWordQuery = (enable: boolean, wordId?: string) => {
+    const dispatch = useDispatch();
     const shouldFetch = enable && !!wordId;
 
-    const detailedWordQuery = useQuery({
-        queryKey: ['word', wordId],
-        queryFn: shouldFetch ? () => wordServices.getById(wordId!) : skipToken,
-        enabled: shouldFetch,
-        refetchOnWindowFocus: false,
-        refetchOnReconnect: false,
-    });
+    const detailedWordQuery = useSuccessQuery(
+        {
+            queryKey: ['word', wordId],
+            queryFn: shouldFetch ? () => wordServices.getById(wordId!) : skipToken,
+            enabled: shouldFetch,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+        },
+        'fetchingDetailedWord',
+        () => dispatch(toNextStep())
+    );
 
     return { detailedWordQuery };
 };
