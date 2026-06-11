0b1890dd8c87a73b00369fd1cf4038de291174cb
little_q_exist <3061982805@qq.com>
2026-06-11 17:35:08 +0800

    feat: 在登出按钮组件增加登出请求

--- src/modules/auth/components/LogoutButton.tsx
+++ src/modules/auth/components/LogoutButton.tsx
@@ -1,27 +1,33 @@
 import { useDispatch } from 'react-redux';
 import { logout } from '@/features/userSlice';
+import authService from '@modules/auth/services/auth';
+import { useMutation } from '@tanstack/react-query';
 
 interface LogoutButtonProps {
-    extraFn: () => void;
+    extraFn?: () => void;
 }
 
 const LogoutButton = ({ extraFn }: LogoutButtonProps) => {
     const dispatch = useDispatch();
 
+    const logoutMutation = useMutation({
+        mutationFn: authService.logout,
+        mutationKey: ['logout'],
+        retry: 3,
+    });
+
     return (
-        <div>
-            <div>
-                Already have an account?{' '}
-                <a
-                    onClick={() => {
-                        dispatch(logout());
-                        extraFn();
-                    }}
-                >
-                    Log out
-                </a>
-            </div>
-        </div>
+        <a
+            onClick={() => {
+                logoutMutation.mutate();
+                dispatch(logout());
+                if (extraFn) {
+                    extraFn();
+                }
+            }}
+        >
+            Log out
+        </a>
     );
 };
 
