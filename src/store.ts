25190cb13754a38b82bb6c034a635b523f201a3a
little_q_exist <3061982805@qq.com>
2026-06-12 09:43:52 +0800

    fix: 移除store中authSlice的注册

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/userSlice';
-import authReducer from './features/authSlice';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
-        auth: authReducer,
     },
 });
 
